a5fca3ac31f13a4bd27479e697095384e5a71eb6
Aileen CM <aileenchany@gmail.com>
2021-04-28 18:57:27 -0400

    added js functions to fetch api data and display random users

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function() {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    //console.log(data);
+    const userResults = data.results; //gets arrays from object
+    //console.log(usersResults);
+    displayUsers(userResults);
+};
+
+getData();
+
+const displayUsers = function(userResults) {
+    randomFolks.innerHTML = ""; //emptying the element
+
+    for (const user of userResults) {
+        const name = user.name.first;
+        const country = user.location.country;
+        const img = user.picture.medium;
+
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML =
+            `<h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${img} alt="User avatar" />`;
+        randomFolks.append(userDiv);
+    }
+};
